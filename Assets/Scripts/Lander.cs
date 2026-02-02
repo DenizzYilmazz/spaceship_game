b80b53b44f7282e3fd70f582488b6d2d87793901
Deniz Yılmaz <den1zkz@yandex.com>
2026-02-03 00:05:18 +0300

    Initial commit

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Rigidbody2D landerRigidbody2D;
+
+    private void Awake()
+    {
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            float force = 700f;
+            landerRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
+            Debug.Log("Up");
+        }
+        
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            float turnSpeed = +100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            Debug.Log("Left");
+        }
+        
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            float turnSpeed = -100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+            Debug.Log("Right");
+        }
+    }
+}
